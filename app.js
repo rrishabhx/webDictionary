db71a597b70b2419ce6b2e68ea10fd7494cc1944
Risabh Raj <ratedrrisabh@gmail.com>
2018-10-14 17:54:35 +0530

    Style user interface

--- app.js
+++ app.js
@@ -1,40 +1,82 @@
-var express     = require("express"),
-    app         = express(),
-    request    = require("request"),
-    API_KEY     = "da4254d7814a3d40182b22afc4f8a7e5862367bd63716f28a";
+var express = require("express"),
+    app = express(),
+    request = require("request"),
+    API_KEY = "da4254d7814a3d40182b22afc4f8a7e5862367bd63716f28a";
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-    res.render("search");
-});
-
-app.get("/results", function (req, res) {
-    var wordToSearch = req.query.word;
-    var url = "https://api.wordnik.com/v4/word.json/" + wordToSearch +
-    "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=" + API_KEY;
-
-    request(url, function (error, response, body) {
+//Root route
+app.get("/", function(req, res) {
+    var url = "https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=" + API_KEY;
+    
+    request(url, function(error, response, body) {
         var data = JSON.parse(body);
 
         if(!error && response.statusCode === 200) {
-            if(data.length === 0){
-                res.send("No results found")
+            if(data.length === 0) {
+                res.send("No results found");
             } else {
-                res.render("results", {
-                    data: data,
-                    word: wordToSearch
+                res.render("search", {data: data});
+            }
+        }
+    });
+
+});
+
+//Results route
+app.get("/results", function(req, res) {
+    var wordToSearch = req.query.word;
+    console.log("Word to search = " + wordToSearch);
+    var resultsJson = {};
+    var urlDef = "https://api.wordnik.com/v4/word.json/" + wordToSearch +
+        "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=" + API_KEY;
+
+    var urlExamples = "https://api.wordnik.com/v4/word.json/" + wordToSearch + 
+        "/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=" + API_KEY;
+
+    console.log("====================================================");
+    console.log("Def URL: " + urlDef);
+    console.log("Examples URL: " + urlExamples);
+    console.log("====================================================");
+
+
+    //GET definitions
+    request(urlDef, function (error, response, body) {
+        var definitions = JSON.parse(body);
+        if (!error && response.statusCode === 200) {
+            if (definitions.length === 0) {
+                res.render("noresults.ejs", {wordToSearch: wordToSearch});
+            } else {
+                resultsJson.definitions = definitions;
+                resultsJson.word = wordToSearch;
+                //GET examples
+                request(urlExamples, function(errorE, responseE, bodyE) {
+                    var exampleData = JSON.parse(bodyE);
+
+                    if (!errorE && responseE.statusCode === 200) {
+                        if (exampleData.length === 0) {
+                            res.send("No results found");
+                        } else {
+                            resultsJson.examples = exampleData["examples"];
+                            console.log("Results Json: " + JSON.stringify(resultsJson));
+                            //Sending response
+                            res.render("results", {
+                                resultsJson: resultsJson
+                            });
+                        }
+                    } else {
+                        console.log("error: ", error);
+                    }
                 });
             }
-            
         } else {
             console.log("error: ", error);
         }
-    })
+    });
 
-})
+});
 
-
-app.listen(process.env.PORT || 8080, process.env.IP || "localhost", function () {
-    console.log("Online dictionary application started...")
+app.listen(process.env.PORT || 8080, process.env.IP || "localhost", function() {
+    console.log("Online dictionary application started...");
 });
